c7bc4eae582efd210c6ae1ad2826733de4ff0bb7
Phil Gale <PhilGale92@users.noreply.github.com>
2025-02-08 13:56:44 +0000

    'x'

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,21 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  distDir: "build",
+  poweredByHeader: false,
+  reactStrictMode: true,
+  generateBuildId: async () => {
+      return new Date().getTime().toString();
+  },
+  images: { unoptimized: true },
+  // FIX: styled-component build support
+  compiler: {
+      // styled components support
+      styledComponents: true,
+      // remove data-testid props (only from builds)
+      reactRemoveProperties: (process.env.NODE_ENV === 'production'),
+  },
 };
 
 export default nextConfig;
